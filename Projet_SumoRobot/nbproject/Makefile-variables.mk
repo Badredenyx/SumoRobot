5c73987704acfc5869cba7d0aaa6a4137843ca18
BadrEddineKARAFLI <badr.eddine.karafli@gmail.com>
2025-05-14 21:34:11 +0200

    Finalized my Project

---
+++ Projet_SumoRobot/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PROJET_SUMO.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PROJET_SUMO.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=projetsumo.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/projetsumo.x.tar
